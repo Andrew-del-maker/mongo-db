c9901b840f6e194379193e200938f6d22f75aeea
Andrew Savchuk <normcause@gmail.com>
2021-11-14 17:29:32 +0200

    l

--- app.js
+++ app.js
@@ -4,6 +4,9 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
+const {DB_HOST} = process.env
 const app = express()
 app.use(cors())
 
@@ -11,10 +14,9 @@ app.get('/', (req, res) => {
     res.send('<h2>Home page</h2>')
 })
 
-const DB_HOST = 'mongodb+srv://Andrew:SbWl49o10joNIlEY@cluster0.9wmzw.mongodb.net/contactList?retryWrites=true&w=majority'
 mongoose.connect(DB_HOST)
     .then(() => {
-        app.listen(3000)
+        app.listen(3002)
     })
     .catch(error => {
         console.log(error.message)
